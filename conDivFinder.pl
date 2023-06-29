a16e32ef0e830668ef4b4b16ba414a81108755a8
jackie-duang <33014706+jackie-duang@users.noreply.github.com>
2023-06-29 14:36:04 +0800

    Update conDivFinder.pl

--- conDivFinder.pl
+++ conDivFinder.pl
@@ -5,11 +5,14 @@ use Getopt::Long;
 my $usage = <<USAGE;
 Usage:
     perl $0 -in <input.fasta> -l <species.list> -w <window size> -con <conserved number> -div <changed number> -bn <background can not conserved species>
-    
+
+    -in input.fasta : sequence aligned in fasta format file
+	-l species.list : species list with species marked
     -w window size : default 20 , the window size for calculating the conserved region
     -con conserved number : default 18 , identify as conserved region if the number of nucleotides in the window is large or equal to this value
     -div changed number : default 12 , identify as changed region if the number of nucleotides in the window is less or equal to this value
     -bn background can not conserved species : default 0 , how many species can not be conserved in the background species
+	
     ---
     species list file format, one species per line and foreground species marked with '*':
     human
