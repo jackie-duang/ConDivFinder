200ac0a18ab61eeec4b1408b59431bbf269c7b40
jackie-duang <33014706+jackie-duang@users.noreply.github.com>
2023-06-29 14:38:07 +0800

    Update conDivFinder.pl

--- conDivFinder.pl
+++ conDivFinder.pl
@@ -7,7 +7,7 @@ Usage:
     perl $0 -in <input.fasta> -l <species.list> -w <window size> -con <conserved number> -div <changed number> -bn <background can not conserved species>
 
     -in input.fasta : sequence aligned in fasta format file
-	-l species.list : species list with species marked
+    -l species.list : species list with species marked 
     -w window size : default 20 , the window size for calculating the conserved region
     -con conserved number : default 18 , identify as conserved region if the number of nucleotides in the window is large or equal to this value
     -div changed number : default 12 , identify as changed region if the number of nucleotides in the window is less or equal to this value
